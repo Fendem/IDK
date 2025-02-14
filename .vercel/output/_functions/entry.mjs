cec818abf2af2dddfd83bce0f8a34595f8e801b7
FERDI123456789 <ferdi0909lol@pm.me>
2025-02-14 19:21:38 +0100

    Hosted it

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from "./renderers.mjs";
 import { c as createExports } from "./chunks/entrypoint_DrPZA5tF.mjs";
-import { manifest } from "./manifest_DJJe19jR.mjs";
+import { manifest } from "./manifest_DFWMW_8r.mjs";
 const serverIslandMap = /* @__PURE__ */ new Map();
 ;
 const _page0 = () => import("./pages/_image.astro.mjs");
@@ -20,7 +20,7 @@ const _manifest = Object.assign(manifest, {
   middleware: () => import("./_astro-internal_middleware.mjs")
 });
 const _args = {
-  "middlewareSecret": "cfcd28eb-69a2-4dc4-b611-c0b75b425c50",
+  "middlewareSecret": "3333ca51-e6f8-45b1-944b-0fe110237182",
   "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
