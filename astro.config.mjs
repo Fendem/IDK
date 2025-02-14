33af871f0dafead67ab478c01f42afd30c8d3c5b
FERDI123456789 <ferdi0909lol@pm.me>
2025-02-14 19:16:49 +0100

    Hosted it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node'
 import react from '@astrojs/react'
 import clerk from '@clerk/astro'
 import tailwind from '@astrojs/tailwind';
