a5a03a74699bccb5916a7de39e823fc89c71a4ae
FERDI123456789 <ferdi0909lol@pm.me>
2025-02-22 01:27:37 +0100

    Clerk now intergraded

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from '@astrojs/react'
 import clerk from '@clerk/astro'
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
-import { dark, neobrutalism } from '@clerk/themes'
 
 export default defineConfig({
   integrations: [react(), clerk({
